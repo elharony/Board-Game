5981d7751c422b9fad8d1ad6a9638c1a9036fbde
elharony <yahya@elharony.com>
2019-05-17 16:49:38 +0200

    Disable grid items

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,8 +19,28 @@ function createGridItem() {
 
 
 /**
+ * Disabled Grid Items
+ */
+function disableGridItems() {
+    const disabledItems = 15;
+
+    for(let i = 0; i < disabledItems; i++) {
+        grid.childNodes[getRandomInt(0, 99)].classList.add('disabled')
+    }
+}
+
+// SOF: https://stackoverflow.com/a/1527820/5560399
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+ 
+/**
  * Init
  */
 (function init() {
     drawGrid();
+    disableGridItems();
 })()
