aad322bd7dfbf70eca15d61a4d17bf9e937bd025
elharony <yahya@elharony.com>
2019-06-26 20:03:49 +0200

    Check for nearby 'weapons/players' before placing a weapon

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ let unavailableCells = [];
  * 1. Draw the grid
  * 2. Place `dimmed` cells
  * 3. Place `weapons`
- * 4. Place 2 `players` randomaly
+ * 4. Place 2 `players`
  */
 
 
@@ -34,12 +34,56 @@ function createGridItem(row, col) {
 
 
 /**
+ * Nearby Cells 
+ */
+function checkNearby(row, col) {
+
+    const avoidItems = ['weapon-attack', 'weapon-defense', 'player-police', 'player-thief'];
+
+    /* Reset row/col number if reaches the least/maximum */
+    row = (row == 0) ? 1 : row;
+    row = (row == 9) ? 8 : row;
+    col = (col == 0) ? 1 : col;
+    col = (col == 9) ? 8 : col;
+
+    /* Check nearby cells */
+
+    /**
+     * Example: (5, 6)
+     *
+     *          [4, 6]
+     *   [5, 5] [5, 6] [5, 7]
+     *          [6, 6] 
+     */
+    let topCell = document.querySelector(`.cell_${row - 1}_${col}`);
+    let bottomCell = document.querySelector(`.cell_${row + 1}_${col}`);
+    let rightCell = document.querySelector(`.cell_${row}_${col + 1}`);
+    let leftCell = document.querySelector(`.cell_${row}_${col - 1}`);
+    
+    // Check for nearby weapons
+    for(let item = 0; item < avoidItems.length; item++) {
+        if(
+            topCell.classList.contains(avoidItems[item]) ||
+            bottomCell.classList.contains(avoidItems[item]) ||
+            rightCell.classList.contains(avoidItems[item]) ||
+            leftCell.classList.contains(avoidItems[item])
+        ) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+
+/**
  * isAvailableCell?
  */
 function isAvailableCell(row, col) {
     let result = !(unavailableCells.includes(`cell_${row}_${col}`)); // `!` To get "Not Available"
     return result;
 }
+
 
 /**
  * Place Item
@@ -106,7 +150,18 @@ function placeWeapons() {
         // We've found an available cell
         if(isAvailableCell(randCellRow, randCellCol)) {
 
-            placeItem(randCellRow, randCellCol, weapon.className);
+            // Is there any nearby weapons?
+            if(checkNearby(randCellRow, randCellCol)) {
+
+                placeItem(randCellRow, randCellCol, weapon.className);
+
+            } else {
+
+                // Try again!
+                return addWeapon(weapon);
+
+            }
+            
 
         } else {
             // Try again!
@@ -188,6 +243,6 @@ function getRandomInt(min, max) {
 (function init() {
     drawGrid();
     placeDisabledCells();
-    placeWeapons();
     placePlayers();
+    placeWeapons();
 })()
